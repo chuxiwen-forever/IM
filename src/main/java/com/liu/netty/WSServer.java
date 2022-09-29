3d8c6d35e86c473d73ef99f197c33d523e431c58
chuxiwen <1609185446@qq.com>
2022-09-29 08:13:22 +0800

    SpringBoot 整合 netty

--- src/main/java/com/liu/netty/WSServer.java
+++ src/main/java/com/liu/netty/WSServer.java
@@ -5,23 +5,38 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.springframework.stereotype.Component;
 
+@Component
 public class WSServer {
-    public static void main(String[] args) throws Exception {
-        EventLoopGroup mainGroup = new NioEventLoopGroup();
-        EventLoopGroup subGroup = new NioEventLoopGroup();
 
-        try {
-            ServerBootstrap server = new ServerBootstrap();
-            server.group(mainGroup,subGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .childHandler(new WSServerInit());
+    private EventLoopGroup mainGroup;
+    private EventLoopGroup subGroup;
+    private ServerBootstrap server;
+    private ChannelFuture future;
 
-            ChannelFuture future = server.bind(8088).sync();
-            future.channel().closeFuture().sync();
-        } finally {
-            mainGroup.shutdownGracefully();
-            subGroup.shutdownGracefully();
-        }
+    private static class SingletonWSServer{
+        static final WSServer instance = new WSServer();
     }
+    public static WSServer getInstance(){
+        return SingletonWSServer.instance;
+    }
+
+
+    private WSServer(){
+        mainGroup = new NioEventLoopGroup();
+        subGroup = new NioEventLoopGroup();
+        server = new ServerBootstrap();
+        server.group(mainGroup,subGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new WSServerInit());
+
+    }
+
+    public void start(){
+        future = server.bind(8088);
+        System.err.println("服务器启动成功...");
+    }
+
+
 }
